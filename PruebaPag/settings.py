0704a0bd1f81eb4064261c025b49eb0973561382
crist <cristianvillalobos666@gmail.com>
2023-12-04 21:58:54 -0300

    esta siii

--- PruebaPag/settings.py
+++ PruebaPag/settings.py
@@ -24,11 +24,14 @@ STATIC_DIR=os.path.join(BASE_DIR,'static')
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY',default='your secret key')
 
-DEBUG = os.environ.get("DEBUG","False").lower()=="true"
+DEBUG = 'RENDER' not in os.environ
 
 # SECURITY WARNING: don't run with debug turned on in production!
+ALLOWED_HOSTS = []
 
-
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Application definition
 
