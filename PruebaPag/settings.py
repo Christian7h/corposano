e3e5b172683d111fba42f05e086be6bd586dcebc
crist <cristianvillalobos666@gmail.com>
2023-12-04 21:28:23 -0300

    change setting3

--- PruebaPag/settings.py
+++ PruebaPag/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY',default='your secret key')
 DEBUG = os.environ.get("DEBUG","False").lower()=="true"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-ALLOWED_HOSTS=os.environ.get("ALLOWED_HOSTS").split("")
+ALLOWED_HOSTS=os.environ.get("ALLOWED_HOSTS").split(" ")
 
 ALLOWED_HOSTS = []
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
