bd39d4c49ffedb9482281fa1b6bc82de3f133fd2
crist <cristianvillalobos666@gmail.com>
2023-12-04 21:33:50 -0300

    change setting4

--- PruebaPag/settings.py
+++ PruebaPag/settings.py
@@ -29,7 +29,6 @@ DEBUG = os.environ.get("DEBUG","False").lower()=="true"
 # SECURITY WARNING: don't run with debug turned on in production!
 ALLOWED_HOSTS=os.environ.get("ALLOWED_HOSTS").split(" ")
 
-ALLOWED_HOSTS = []
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
